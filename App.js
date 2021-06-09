41b91ff3a34450c4e0d0b46731c5529c6f4e421d
priyanshu2000 <priyanshubhojane685@gmail.com>
2021-06-10 03:43:53 +0530

    added app icon & splashscreen

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import React, { useEffect } from 'react'
 import Navigator from './navigators'
-import { ToastConfig } from './components/toast-message'
 import Toast from 'react-native-toast-message'
+import SplashScreen from 'react-native-splash-screen'
+import NetWorkStatus from './components/network-status'
+import { ToastConfig } from './components/toast-message'
+import { NavigationContainer } from '@react-navigation/native'
 
 const App = () => {
+
+  useEffect(() => {
+        SplashScreen.hide()
+  }, [])
+
   return (
     <NavigationContainer>
        <Navigator/>
+       <NetWorkStatus/>
        <Toast config={ToastConfig} ref={(ref) => Toast.setRef(ref)}/>
     </NavigationContainer>
   )
