f45e4a566e2a610356926f0b9ddff995867ba088
priyanshu2000 <priyanshubhojane685@gmail.com>
2021-06-10 04:00:05 +0530

    added header component & floating action button

---
+++ components/header.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, StatusBar } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons'
+import colors from '../constants/colors';
+
+const Header = ({ isShowIcons , right , isShowBack  }) => {
+
+    const { goBack } = useNavigation()
+
+    if(isShowIcons) return (
+                    <>
+                        <StatusBar backgroundColor={colors.Primary} />
+                            <View style={styles.headerContainer} >
+                                { isShowBack && <TouchableOpacity onPress={() => goBack()} style={styles.backIcon} >
+                                                <Icon name='chevron-back-outline' size={20} color={colors.White}  />
+                                                </TouchableOpacity>}
+                                <View style={styles.headerTextContainer} >
+                                    <Text style={styles.headerText}>Clippy</Text>
+                                </View>
+                                { right && <View style={styles.right}>{right}</View>}
+                            </View>
+                    </>
+    )
+
+    else return (
+        <>
+            <StatusBar backgroundColor={colors.Primary} />
+            <View style={styles.headerContainer} >
+                <View style={styles.headerTextContainer} >
+                    <Text style={styles.headerText} >Clippy</Text>
+                </View>
+            </View>
+        </>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    headerContainer:{
+        width:'100%',
+        justifyContent:'center',
+        backgroundColor:colors.Primary,
+        position:'relative',
+        shadowColor: colors.Black,
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 0.4,
+        shadowRadius: 1,
+        elevation: 2,
+        paddingVertical: 15,
+        zIndex:10
+    },
+    backIcon:{
+        position: 'absolute',
+        zIndex: 1,
+        marginHorizontal:5
+    },
+    right:{
+        position: 'absolute',
+        zIndex: 1,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerTextContainer:{
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    headerText:{
+        fontFamily:'MediumItalic',
+        fontSize: 20,
+        color:colors.White,
+    },
+})
+
+export default Header;
+
